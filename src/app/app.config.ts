6644651b4b46cb236dd2b388e6eb5f88ce1107c9
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2024-06-11 22:07:56 +0200

    feat: always enable service worker (even in dev)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,12 @@ import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: true, // !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
 };
